e41ab439367ceb8c79d44b81ac9104604678ba26
leonardo <leonardo@ragnar.homenet.telecomitalia.it>
2023-08-13 19:41:06 +0200

    firefox opens on the current tag

--- config.h
+++ config.h
@@ -108,7 +108,7 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "Firefox", NULL,     NULL,           0,    0,          0,          -1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Alacritty" ,      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ TERMCLASS ,      NULL,     NULL,           0,         0,          1,           0,        -1 },
